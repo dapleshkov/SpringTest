d726764e30c30336cb4c66fb9df4197c544ba881
dapleshkov <dapleshkov@edu.hse.ru>
2019-11-10 11:56:50 +0300

    4 guide

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,9 +1,6 @@
 package com.example.sweater.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -14,10 +11,21 @@ public class Message {
     private String text;
     private String tag;
 
+    public String getAuthorName(){
+        return author!=null ? author.getUsername() : "<name>";
+    }
+    public User getAuthor() {
+        return author;
+    }
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
+        this.author = user;
         this.text = text;
         this.tag = tag;
     }
